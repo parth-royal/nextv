476a3a3783db6c2eebcd798e834eac8d15782938
2023emi <you@example.com>
2024-05-20 08:17:20 +0530

    01

---
+++ _next/static/_kzWBNJyq64WKOgn1ywfO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/664-806b52bf69b97e98.js","static/chunks/pages/index-237895add39615c8.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
